1dbeeb410f899dd9e8a15619ca708e625d6b1207
Katrina Owen <katrina.owen@gmail.com>
2013-07-27 20:22:53 -0600

    Ensure submission tests work

--- test/exercism/submission_test.rb
+++ test/exercism/submission_test.rb
@@ -2,15 +2,27 @@ require './test/test_helper'
 require 'exercism/submission'
 
 class SubmissionTest < Minitest::Test
-  def test_knows_ruby
+  def test_knows_ruby_code
+    refute Exercism::Submission.test?('queens.rb')
+  end
+
+  def test_identifies_ruby_tests
     assert Exercism::Submission.test?('queens_test.rb')
   end
- 
-  def test_knows_elixir
+
+  def test_knows_elixir_code
+    refute Exercism::Submission.test?('queens.exs')
+  end
+
+  def test_identifies_elixir_tests
     assert Exercism::Submission.test?('queens_test.exs')
   end
 
-  def test_knows_javascript
+  def test_knows_javascript_code
+    refute Exercism::Submission.test?('queens.js')
+  end
+
+  def test_identifies_javascript_tests
     assert Exercism::Submission.test?('queens.spec.js')
   end
 end
