b2e255778f36ab20d5c9b4d8bb5e706579ca1774
Katrina Owen <katrina.owen@gmail.com>
2013-07-27 20:22:59 -0600

    Refactor submission

--- lib/exercism/submission.rb
+++ lib/exercism/submission.rb
@@ -2,22 +2,26 @@ class Exercism
   class Submission
 
     def self.test?(file)
-      submission = new(file)
-      submission.test?
+      new(file).test?
     end
 
     attr_reader :file
-   
     def initialize(file)
       @file = file
     end
- 
+
     def test?
-      test_identifiers.any? {|_, test_suffix| file.end_with?(test_suffix)}
+      test_identifiers.any? do |_, suffix|
+        file.end_with?(suffix)
+      end
     end
 
     def test_identifiers
-      { :ruby => '_test.rb', :js => '.spec.js', :elixir => '_test.exs' }
+      {
+        :ruby => '_test.rb',
+        :js => '.spec.js',
+        :elixir => '_test.exs'
+      }
     end
 
   end
